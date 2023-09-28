515b875530406dd03112a28440029d775cc68d5c
Sonurxl423 <sonukrrxl320@gmail.com>
2023-09-28 16:08:34 +0530

    resolve errors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import Textform from "./components/Textform";
+import React, { useState } from "react";
+
+import "./App.css";
+import Alert from "./components/Alert";
+import About from "./components/About";
 
 function App() {
+  const [mode, setMode] = useState("white");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
+  const toggleMode = () => {
+    if (mode === "white") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#242e7c";
+      showAlert("Dark Mode has been enabled", "success");
+      document.title = "Textutils-DarkMode";
+    } else {
+      setMode("white");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode has been enabled", "success");
+      document.title = "Textutils-LightMode";
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Navbar/> */}
+      <Navbar titles="Textutiles" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      {/* 
+          <Switch>
+            <Route path="/about">
+             
+            </Route>
+            <Route path="/">
+              
+            </Route>
+          </Switch>
+         */}
+      <div className="container my-3">
+        <Textform
+          showAlert={showAlert}
+          heading="Enter the text to analyze the text"
+          mode={mode}
+        />
+      </div>
+      <About />
+
+    </>
   );
 }
 
---
+++ src/components/Textform.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+
+export default function Textform(props) {
+  // setText("New text can be written here .");
+  const [text, setText] = useState("");
+
+
+  const handleupclick = () => {
+    // console.log("Uppercase botton is clicked"+ text);
+    let newText=text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to UpperCase ","success");
+  };
+
+  const handleloclick = () => {
+    // console.log("Uppercase botton is clicked"+ text);
+    let newText=text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to LowerCase ","success");
+  };
+
+
+  const handleOnChange = (event) => {
+    // console.log("On change");
+    setText(event.target.value);
+  };
+
+
+  return (
+    <>
+    <div style={{color:props.mode==='dark'?'white':'black'}}>
+      <h1>{props.heading}</h1>
+      <div className="mb-3" >
+        <textarea
+          className="form-control"
+          value={text}
+          onChange={handleOnChange}
+          style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'black'}}
+          
+          id="exampleFormControlTextarea1"
+          rows="8"
+        ></textarea>
+        <button className="btn bg-primary my-3 " onClick={handleupclick}>
+          Convert to uppercase
+        </button>
+        <button className="btn bg-primary my-3 mx-1" onClick={handleloclick}>
+          Convert to lowercase
+        </button>
+      </div>
+    </div>
+    <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
+      <h1>Your text summary here</h1>
+      <p>{text.split(" ").length-1} words and {text.length} characters</p>
+      <p>{0.008*text.split(" ").length} Minutes read</p>
+      <h2>Preview</h2>
+      <p>{text.length>0?text:"Enter something in the above textbox to preview it here "}</p>
+
+    </div>
+    </>
+  );
+}
